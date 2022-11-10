139bf420940aa833170e9bfe386aad4a1c54bb7a
josecausil <85271485+josecausil@users.noreply.github.com>
2022-11-10 10:53:43 -0500

    Finally1.5

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
-
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
  
  public static void main(String[] args){
     Scanner entrada = new Scanner(System.in);
-    ArrayList<Cliente> listaNumber ;
+    List<Cliente> listaNumber ;
     String numero;
     boolean value;
     int opcion=0;
